7b6f73ae2a69b8511b3bdb29dc8fbe1f64e58256
Albarracinalejandro <103397954+Albarracinalejandro@users.noreply.github.com>
2023-04-12 13:40:51 -0500

    se agregó la función aFecha

--- funciones.js
+++ funciones.js
@@ -16,3 +16,19 @@ function aCelular(numero) {
   // Devolver el número con espacios
   return numeroConEspacios;
 }
+
+function aFecha(dateString) {
+  // necesita una string de tipo "21/2/2023 10:06:38" para devolver una string de tipo "21 de febrero de 2023, 10:06am";
+  let months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+  let dateParts = dateString.split(/[\/ :]/);
+  let year = parseInt(dateParts[2]);
+  let month = parseInt(dateParts[1]) - 1;
+  let day = parseInt(dateParts[0]);
+  let hours = parseInt(dateParts[3]);
+  let minutes = parseInt(dateParts[4]);
+  let ampm = hours >= 12 ? 'pm' : 'am';
+  let formattedHours = hours % 12 || 12;
+  let formattedMinutes = minutes.toString().padStart(2, '0');
+  let formattedDate = `${day} de ${months[month]} de ${year}, ${formattedHours}:${formattedMinutes}${ampm}`;
+  return formattedDate;
+}
